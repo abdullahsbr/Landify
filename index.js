59405f8ceb8252a7ed35f8c0964c14d05ab26701
unknown <abdullahs68400@gmail.com>
2025-01-01 16:50:49 +0300

    resposive edit

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("menuToggle").addEventListener("click", function () {
-  var navLinks = document.getElementById("navLinks");
-  navLinks.classList.toggle("active"); // Menüyü açık/kapalı durumuna getir
-});
